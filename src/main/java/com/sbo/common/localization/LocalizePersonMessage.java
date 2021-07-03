34b394424a58848c5ab04909deacbbd067dfc992
vicras <victor.graskov@gmail.com>
2021-07-03 15:01:36 +0300

    feat: refactor code after pull

---
+++ src/main/java/com/sbo/common/localization/LocalizePersonMessage.java
@@ -0,0 +1,29 @@
+package com.sbo.common.localization;
+
+import com.sbo.provider.CurrentPersonProvider;
+import lombok.AllArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author viktar hraskou
+ */
+@Component
+@AllArgsConstructor
+public class LocalizePersonMessage {
+
+    private final MessageSource messageSource;
+    private final CurrentPersonProvider personProvider;
+
+    public String getText(String key, String... args) {
+        var currentPerson = personProvider.getCurrentPerson();
+        var locale = currentPerson.getLanguage().getLocale();
+        return messageSource.getMessage(key, args, locale);
+    }
+
+    public String getTextOrElse(String key, String defaultMessage, String... args) {
+        var currentPerson = personProvider.getCurrentPerson();
+        var locale = currentPerson.getLanguage().getLocale();
+        return messageSource.getMessage(key, args, defaultMessage, locale);
+    }
+}
